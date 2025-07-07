17eadafb19d5e6e43e4f5f60aa7c3226a1852ca3
w1ze55 <cacauezin@gmail.com>
2025-07-07 17:01:07 -0300

    Feat: Added POPUP message for system

--- QRPix/src/components/Form.vue
+++ QRPix/src/components/Form.vue
@@ -8,6 +8,19 @@ const valor = ref('');
 const descricao = ref('');
 const qrCodeUrl = ref('');
 
+const mostrarPopUp = ref(false);
+const mensagemPopup = ref('');
+const tipoPopup = ref('erro');
+
+function mostrarMensagem(mensagem: string, tipo: string='erro') {
+    mostrarPopUp.value = true;
+    mensagemPopup.value = mensagem;
+    tipoPopup.value = tipo;
+}
+
+setTimeout(() => {
+    mostrarPopUp.value = false;
+}, 3000);
 
 
 </script>
