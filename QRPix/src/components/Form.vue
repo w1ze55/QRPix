4dadc440ca068ee778b721640b53192a99caff90
w1ze55 <cacauezin@gmail.com>
2025-07-07 16:56:03 -0300

    reformalted structure and added variables for (form)

---
+++ QRPix/src/components/Form.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const chavePix = ref('');
+const nome = ref('');
+const cidade = ref('');
+const valor = ref('');
+const descricao = ref('');
+const qrCodeUrl = ref('');
+
+
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped></style>
